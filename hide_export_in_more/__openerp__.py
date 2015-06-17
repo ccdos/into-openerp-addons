559835cff4b0954257543814c94f54de2bcf9eff
ccdos <ccdos@163.com>
2015-06-17 13:16:40 +0800

    隐藏 “更多" 菜单里的 “导出” 按钮

---
+++ hide_export_in_more/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Hide Export in More Option',
+    'version': '1.0',
+    'category': 'Base',
+    'author': 'CCDOS',
+    'website': 'http://www.intoerp.com',
+    'description': """
+
+Hide Export in More Option in OpenERP-7
+
+	""",
+    'depends': ['web'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'web_preload': True,
+    # 'js': ['static/src/js/none.js'],
+    'qweb': [
+        "static/src/base.xml",
+    ]
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ hide_export_in_more/controllers/main.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+
+# vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
