d46e5b00f8be6671659f0beeb5a46da370506a24
ccdos <ccdos@163.com>
2015-08-26 01:24:34 +0800

    升级， 根据 test_need_approval 的结果， 进一步增加 对特殊条件的订单，需通过上级人员 首先批准，然后才可以 安排生产

--- sale_double_validation/__openerp__.py
+++ sale_double_validation/__openerp__.py
@@ -11,15 +11,15 @@
     'name' : 'Double Validation on Sale',
     'version' : '1.1',
     'category': 'Sale Management',
-    'depends' : ['base','sale'],
-    'author' : 'OpenERP SA',
+    'depends': ['base', 'sale_stock'],
+    'author': 'ccdos@intoerp.com 单雷平',
     'description': """
 销售订单的 两次确认
 =========================================================
 销售人员 确认订单后，状态为  waiting_date
 
 然后 应由 计划部（生产部） 人员 在此确认批准后，才会安排生产，
-可扩充为 符合 一定条件才能被批准
+如果不满足条件， 必须经 由 生产副总经理 才能被批准
 
     """,
     'website': 'http://www.intoerp.com',
--- sale_double_validation/sale_order.py
+++ sale_double_validation/sale_order.py
@@ -5,15 +5,29 @@
 #
 ##############################################################################
 from osv import fields, osv
-from datetime import time, datetime
-from openerp.tools.translate import _
-from ast import literal_eval
-from dateutil.relativedelta import relativedelta
 from openerp import netsvc
 
 class sale_order(osv.osv):
     _inherit = "sale.order"
 
+    _columns = {
+        'state': fields.selection([
+            ('draft', 'Draft Quotation'),
+            ('sent', 'Quotation Sent'),
+            ('cancel', 'Cancelled'),
+            ('waiting_date_approval', 'Waiting Approval'),
+            ('waiting_date', 'Waiting Schedule'),
+            ('progress', 'Sales Order'),
+            ('manual', 'Sale to Invoice'),
+            ('shipping_except', 'Shipping Exception'),
+            ('invoice_except', 'Invoice Exception'),
+            ('done', 'Done'),
+        ], 'Status', readonly=True, track_visibility='onchange',
+            help="Gives the status of the quotation or sales order.\
+              \nThe exception status is automatically set when a cancel operation occurs \
+              in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
+               but waiting for the scheduler to run on the order date.", select=True),
+    }
     def action_cancel_draft(self, cr, uid, ids, context=None):
         if not len(ids):
             return False
@@ -25,19 +39,28 @@ class sale_order(osv.osv):
             wf_service.trg_create(uid, 'sale.order', p_id, cr)
         return True
 
+    def test_need_approval(self, cr, uid, ids):
+        """
+        @return: True or False
+        检查 订单是否满足批准的条件 由 不同的  由 不同的  Signal 触发， 这样的 同样的 源activity  和 目的 activity，
+        可以有多个 不同的 transition，
+
+        使用场景示例： 需要审批的订单（返回 True）， 必须由  总经理/副总经理 进行批准
+                      否则（返回False）， 由 生产计划员可以直接批准
+        """
+        res = True
+        # for production in self.browse(cr, uid, ids):
+        #     boms = self._action_compute_lines(cr, uid, [production.id])
+        #     res = False
+        #     for bom in boms:
+        #         product = self.pool.get('product.product').browse(cr, uid, bom['product_id'])
+        #         if product.type in ('product', 'consu'):
+        #             res = True
+        return res
+
     def check_validity(self, cr, uid, ids):
         """ 检查 订单 是否允许被 安排生产
         @return: True or False    默认直接允许生成，如果 需要完整实现，继承此方法扩展实现，示例见后面的备注
         """
         # raise osv.except_osv(_('批准订单时!'), _('存在 C 类商品，必须由生产副总进行批准!') )
         return True        
-
-    # def check_validity(self, cr, uid, ids):
-        # """ 检查 订单 是否允许被 安排生产
-        # @return: True or False
-        # """
-        # for record in self.browse(cr, uid, ids):
-            # today_date = datetime.today().strftime('%Y-%m-%d')
-            # if 不符合要求 就抛错:
-                # raise osv.except_osv(_('批准订单时!'), _('存在 C 类商品，必须由生产副总进行批准!') )
-        # return True   
