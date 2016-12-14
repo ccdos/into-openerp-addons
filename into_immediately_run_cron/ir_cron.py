6fbde61fcc29c89bd699dc0b8684b85d1ef9a33f
ccdos <ccdos@163.com>
2016-12-14 09:31:50 +0800

    [fix] into_immediately_run_cron 返回值

--- into_immediately_run_cron/ir_cron.py
+++ into_immediately_run_cron/ir_cron.py
@@ -11,7 +11,9 @@ _logger = logging.getLogger(__name__)
 class ir_cron(osv.osv):
     _inherit = "ir.cron"
 
-    def Immediately_run_cron(self, cr, uid, ids, context=None):
+    def immediately_run_cron(self, cr, uid, ids, context=None):
         jobs = self.browse(cr, uid, ids, context=context)
         for job in jobs:
             self._callback(cr, job.user_id.id, job.model, job.function, job.args, job.id)
+
+        return True
